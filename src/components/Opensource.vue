acbcc1b8cbf632e204abd4b8c388924f0a4a71dc
Yaty <yaty@live.fr>
2017-09-16 18:55:44 +0200

    i18n

--- src/components/Opensource.vue
+++ src/components/Opensource.vue
@@ -1,6 +1,6 @@
 <template>
   <container id="opensource">
-    <h2 class="title">Open source contributions</h2>
+    <h2 class="title">{{ $t('opensource') }}</h2>
     <div class="columns is-centered">
       <div class="column is-narrow">
         <a class="image is-128x128" href="https://github.com/rafaelpimpa/buefy" target="_blank">

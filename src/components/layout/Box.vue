f2d07dfa00e29988ca185aa0b98b11fa4a2e466d
Yaty <yaty@live.fr>
2017-09-19 19:09:44 +0200

    Old license removed

--- src/components/layout/Box.vue
+++ src/components/layout/Box.vue
@@ -1,11 +1,3 @@
-<!--
-Copyright (C) Hugo Da Roit <contact@hdaroit.fr> - All Rights Reserved
-Unauthorized copying of this file, via any medium is strictly prohibited
-Proprietary and confidential
-Written by Hugo Da Roit <contact@hdaroit.fr>, 2017
-Based on Vue-admin from Fangdun Cai <cfddream@gmail.com>
--->
-
 <template>
   <div class="column is-half">
     <div class="box">

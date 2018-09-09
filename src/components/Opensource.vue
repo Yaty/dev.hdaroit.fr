607e89fa724f72c9b17ea1320e10c5d034cfb82c
Hugo Da Roit <contact@hdaroit.fr>
2018-09-09 13:08:51 +0200

    Add Loopback in opensource projects

--- src/components/Opensource.vue
+++ src/components/Opensource.vue
@@ -4,6 +4,7 @@
     <div class="columns is-centered is-8 is-variable">
       <image-column label="Buefy" link="https://github.com/rafaelpimpa/buefy" img="/static/img/buefy.png"></image-column>
       <image-column label="Vue" link="https://github.com/vuejs/vue" img="/static/img/vue.svg"></image-column>
+      <image-column label="Loopback" link="https://github.com/strongloop/loopback" img="/static/img/loopback.png"></image-column>
     </div>
   </container>
 </template>
